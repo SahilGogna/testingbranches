f0e8d05ae5e9a27e70505ee5e64b20879cc0a927
Sahil Gogna <gogna.sahil95@gmail.com>
2020-06-06 00:36:24 -0400

    1

---
+++ p4.py
@@ -0,0 +1,16 @@
+birthdays = {'Sahil':'July 1', 'Aastha':'Nov 6'}
+
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+       print(birthdays[name] + ' is the birthdate of ' + name)
+    else:
+       print('I do not have birthday information for ' + name)
+       print('What is their birthday?')
+       bday = input()
+       birthdays[name] = bday
+       print('Birthday database updated.')
